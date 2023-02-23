5516fa006ebddf296d3c07ca237b66f77443120e
Maschetti <mateusvmaschietto@gmail.com>
2023-02-23 15:48:57 -0300

    chore: add substituicaoSeleccao to IBVCF1

--- IBVCF1/ibvcf1.c
+++ IBVCF1/ibvcf1.c
@@ -1,5 +1,8 @@
 #include "ibvcf1.h"
 
-void IBVCF1() {
-  
+void IBVCF1(FILE* arquivo, int numeroAlunos) {
+  // inicia as fitas
+  Fita *fitas = iniciaFitas();
+  // coloca os valores de entrada na fita
+  substituicaoSelecao(arquivo, fitas, numeroAlunos);
 }
--- IBVCF1/ibvcf1.h
+++ IBVCF1/ibvcf1.h
@@ -1,7 +1,9 @@
 #ifndef IBVCF1_H
 #define IBVCF1_H
 #include <stdio.h>
+#include "../Functions/functions.h"
+#include "../TAD/tad.h"
 
-void IBVCF1();
+void IBVCF1(FILE *arquivo, int numeroAlunos);
 
 #endif
